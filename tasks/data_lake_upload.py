37367530587c03267527cfead929572f80ce1c59
Sean Wiesner <seanwiesner@MacBook-Air.local>
2023-08-04 02:13:55 -0400

    finished creating upload to s3 python script

--- tasks/data_lake_upload.py
+++ tasks/data_lake_upload.py
@@ -1,7 +1,49 @@
 import os
 import boto3
 from dotenv import dotenv_values
+from botocore.exceptions import NoCredentialsError
 
 
 def data_lake_upload():
-    pass
+    # load config
+    script_path = os.getcwd()
+    config = dotenv_values(f"{script_path}/configuration.env")
+
+    # get upload files
+    files = [f"scraper_scripts/mtgtop8_data.json",
+             f"scraper_scripts/standard_cards.json"]
+
+    # set config variables
+    AWS_BUCKET = config["s3-bucket"]
+
+    def connect_s3():
+        """
+        Create a boto3 session and connect to the S3 Resource
+
+        Returns:
+            connection to the S3 bucket
+        """
+        try:
+            s3_conn = boto3.resource("s3")
+            return s3_conn
+        except NoCredentialsError as e:
+            raise (e)
+
+    def upload_csv_s3():
+        """
+        Upload both CSV files to the S3 bucket
+        """
+        s3_conn = connect_s3()
+        for file in files:
+            s3_conn.meta.client.upload_file(
+                Filename=f"{script_path}/{file}", Bucket=AWS_BUCKET, Key=file)
+
+    upload_csv_s3()
+
+
+def main():
+    data_lake_upload()
+
+
+if __name__ == "__main__":
+    main()
