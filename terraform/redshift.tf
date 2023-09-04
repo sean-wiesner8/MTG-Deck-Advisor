b986652ac81f26d6cf67077a222d4a87e1525846
Sean Wiesner <seanwiesner@dhcp-vl2042-1728.redrover.cornell.edu>
2023-09-03 20:51:16 -0400

    updated redshift database

--- terraform/redshift.tf
+++ terraform/redshift.tf
@@ -7,7 +7,25 @@ resource "aws_db_instance" "mtg_redshift_db" {
   db_name                = "mtg_redshift_db"
   username               = "mtg_project_redshift"
   password               = var.db_password
-  vpc_security_group_ids = [aws_security_group.rds.id]
+  vpc_security_group_ids = [aws_security_group.rds_redshift.id]
   publicly_accessible    = true
   skip_final_snapshot    = true
+}
+
+resource "aws_security_group" "rds_redshift" {
+  name = "mtg-rds-redshift"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
